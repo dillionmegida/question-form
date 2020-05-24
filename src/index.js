6fe0a74a4ee93fcef90079cf8f0fec7b4c441743
Dillion Megida <dillionmegida@gmail.com>
2020-05-24 22:43:26 +0100

    chores: update UI

--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ import styles from "./index.module.scss";
 import QuestionBlock from "./question-block";
 import QuestionPrototype from "./question-prototype";
 import { confirmQuestionFormat, pkgName } from "./utils";
+import PropTypes from "prop-types";
 
-const initialQs = [{ name: "Question 1", options: ["option 1"], answer: 0 }];
-
-const QuestionForm = ({ questions = initialQs, color = "#ddd" }) => {
+const QuestionForm = ({
+    questions = [
+        {
+            name: "What is React?",
+            options: ["React is this", "React is that", "React is cool"],
+            answer: 0,
+        },
+        {
+            name: "What is science?",
+            options: ["Science is bad", "Science is good", "Science is wow"],
+            answer: 2,
+        },
+    ],
+    color = "#ddd",
+}) => {
     if (!Array.isArray(questions)) {
         throw new Error(
             `${pkgName}: 'questions' prop must be of type array, with each question as an object with property name, options and answer`
@@ -167,6 +180,11 @@ const QuestionForm = ({ questions = initialQs, color = "#ddd" }) => {
             </div>
         </section>
     );
+};
+
+QuestionForm.propTypes = {
+    questions: PropTypes.array.isRequired,
+    color: PropTypes.string,
 };
 
 export default QuestionForm;
